563d38e6811f7f34253dc4587c903e06d377fa09
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-01-17 18:27:07 -0400

    Deploy ML Heroes to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ml-heroes/',  // Asegúrate que coincida con tu repo
+  base: "./",  // IMPORTANTE: ./ para GitHub Pages
   build: {
-    outDir: 'dist',
+    outDir: "docs",
     sourcemap: false,
     emptyOutDir: true
   }

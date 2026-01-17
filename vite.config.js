b8e8ffdefdaca81fe92bf0430369ea0f053ecb44
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-01-17 18:16:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ml-heroes/',  // ¡IMPORTANTE! Esto resuelve las rutas
+  base: '/ml-heroes/',
   build: {
     outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) return 'assets/[name]-[hash].css'
-          return 'assets/[name]-[hash][extname]'
-        },
+        assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js'
       }

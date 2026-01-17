a3edb59f30fcf83b1c243f1eddf2b9e225e5b901
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-01-17 17:56:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ml-heroes/',  // Asegúrate que coincida con tu repo
+  base: '/ml-heroes/',  // ← ¡EXACTO nombre de tu repo!
   build: {
     outDir: 'dist',
     sourcemap: false,

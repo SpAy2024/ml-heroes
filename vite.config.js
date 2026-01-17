e9584a847cbfcd21a506ff877e5b895b7ddde0b7
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-01-17 18:35:41 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-﻿import { defineConfig } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: "./",  // IMPORTANTE: ./ para GitHub Pages
-=======
-  base: '/ml-heroes/',
->>>>>>> af6c3ccdfce1b52b7e4cc29c51a006bfdde2dc5b
+  base: "./",  // Para GitHub Pages desde raíz
   build: {
     outDir: "docs",
     sourcemap: false,
